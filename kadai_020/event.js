4cd35ce7e9bc5a01011aa4dfca660f15d9c91d15
hs-kpFjpujN9 <144596996+hs-kpFjpujN9@users.noreply.github.com>
2023-10-06 22:38:34 +0900

    add kadai_020 files

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const textLine= document.getElementById('text');
+const cngBtn= document.getElementById('btn');
+
+cngBtn.addEventListener('click',() => {
+    textLine.textContent ="クリックされました";
+})
